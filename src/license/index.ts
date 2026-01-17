5b89f357a8d7337c343723291624e6de1eda57b7
kiren2548 <kayarat.bam@gmail.com>
2026-01-17 13:32:59 +0700

    แก้

--- src/license/index.ts
+++ src/license/index.ts
@@ -6,28 +6,28 @@ import { error } from "console";
 
 const licenseRoutes = new Hono()
 type license = {
-    licenseID: number
-    type: string
-    expiryDate: string
-    lssueData: string
-    licensenumber: string
+    licenseID: number;
+    type: string;
+    expiryDate: string;
+    lssueData: string;
+    licensenumber: string;
 
 
-}
+};
 
 licenseRoutes.get('/', async (c) => {
     let sql = 'SELECT *FROM license'
-    let stmt = db.prepare(sql)
-    let license = await stmt.all()
+    let stmt = db.prepare(sql);
+    let license = await stmt.all();
 
-    return c.json({ message: 'list of license', data:license })
+    return c.json({ message: 'list of license', data: license ,})
 })
 
 licenseRoutes.get('/:id', (c) => {
     const { id } = c.req.param()
-    let sql = 'SELECT * FROM license WHERE id =@id'
-    let stmt = db.prepare<{ id: string },license>(sql)
-    let license = stmt.get({ id: id })
+    let sql = 'SELECT * FROM license WHERE licenseID =@licenseID'
+    let stmt = db.prepare<{ licenseID: string },license>(sql)
+    let license = stmt.get({ licenseID: id })
 
     if (!license) {
         return c.json({ message: 'license not found' }, 404)
@@ -57,9 +57,9 @@ licenseRoutes.post('/', zValidator('json', createdlicenseShema, (result, c) => {
 }), async (c) => {
     const body = await c.req.json()
     let sql = `INSERT INTO license
-( type, expiryDate, lssueData, licensenumber)
+( "type", expiryDate, lssueData, licensenumber)
 VALUES(@type, @expiryDate, @lssueData, @licensenumber);`
-    let stmt = db.prepare<Omit<license, "id">>(sql)
+    let stmt = db.prepare<Omit<license, "licenseID">>(sql)
     let result = stmt.run(body)
 
     if (result.changes === 0) {
@@ -67,7 +67,7 @@ VALUES(@type, @expiryDate, @lssueData, @licensenumber);`
     }
     let lestRowid = result.lastInsertRowid as number
 
-    let sql2 = 'SELECT * FROM license WHERE id = ?'
+    let sql2 = 'SELECT * FROM license WHERE licenseID = ?'
     let stmt2 = db.prepare<[number], license>(sql2)
     let newlicense = stmt2.get(lestRowid)
     return c.json({ message: 'license created', data: newlicense }, 201)
@@ -83,35 +83,35 @@ licenseRoutes.put(
         const { id } = c.req.param();
         const body = await c.req.json();
 
-        const sqlCheck = 'SELECT * FROM license WHERE id = @id';
-        const stmtCheck = db.prepare<{ id: string }, license>(sqlCheck);
-        const existinglicense = stmtCheck.get({ id });
+        const sqlCheck = 'SELECT * FROM license WHERE licenseID = @licenseID';
+        const stmtCheck = db.prepare<{ licenseID: string }, license>(sqlCheck);
+        const existinglicense = stmtCheck.get({ licenseID:id });
         if (!existinglicense) return c.json({ message: 'license not found' }, 404);
 
         const sql = `
       UPDATE license
-SET "type"=@type, expiryDate=@expiryDate, lssueData=@lssueData, licensenumber=@lssueData
-WHERE licenseID=@id;
+SET type=@type, expiryDate=@expiryDate, lssueData=@lssueData, licensenumber=@lssueData
+WHERE licenseID=@licenseID;
     `;
-        const stmt = db.prepare<Omit<license, 'id'> & { id: string }>(sql);
-        const result = stmt.run({ ...body, id });
+        const stmt = db.prepare<Omit<license, 'licenseID'> & { licenseID: string }>(sql);
+        const result = stmt.run({ ...body, licenseID:id });
 
         if (result.changes === 0) return c.json({ message: 'Failed to update license' }, 500);
 
-        const updatedlicense = stmtCheck.get({ id });
+        const updatedlicense = stmtCheck.get({ licenseID:id  });
         return c.json({ message: 'license updated', data: updatedlicense });
     }
 );
 licenseRoutes.delete('/:id', (c) => {
     const { id } = c.req.param();
-    const sqlCheck = 'SELECT * FROM license WHERE id=@id';
-    const stmtCheck = db.prepare<{ id: string }, license>(sqlCheck);
-    const existinglicense = stmtCheck.get({ id });
+    const sqlCheck = 'SELECT * FROM license WHERE licenseID = @licenseID';
+    const stmtCheck = db.prepare<{ licenseID: string }, license>(sqlCheck);
+    const existinglicense = stmtCheck.get({ licenseID:id });
     if (!existinglicense) return c.json({ message: 'license not found' }, 404);
 
-    const sql = 'DELETE FROM license WHERE id=@id';
-    const stmt = db.prepare<{ id: string }>(sql);
-    stmt.run({ id });
+    const sql = 'DELETE FROM license WHERE licenseID = @licenseID';
+    const stmt = db.prepare<{ licenseID : string }>(sql);
+    stmt.run({ licenseID:id });
 
     return c.json({ message: `license id= ${id} deleted successfully` });
 });
